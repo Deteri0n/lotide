48bc530d3ece69b5b1387bf6765532062216b406
Etienne LC <etlecoent@gmail.com>
2020-09-15 19:35:43 +0000

    Created the assertArrayEqual.js function

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arrayA.length; i++) {
+      if (arrayA[i] !== arrayB[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`✔️ Assertion Passed: ${arrayA} === ${arrayB}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arrayA} !== ${arrayB}`);
+  }
+};
+
+//Test Cases
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
